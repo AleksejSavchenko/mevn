d344b82e340249e79ca0d5a462c28b80a8313db7
Oleksii Savchenko <o.savchenko@andersenlab.com>
2020-03-11 15:07:45 +0200

    design some styles for pages

---
+++ client/pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="my-32">
+    <h1 class="text-center text-gray-800">
+      Mevn Auth
+    </h1>
+  </div>
+</template>
+
--- client/pages/Main.vue
+++ client/pages/Main.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <router-link to="/auth/login">Login</router-link>
-    <router-link to="/auth/register">Register</router-link>
+    <div class="h-2 w-full bg-yellow-600"></div>
+    <div class="h-12 w-full flex items-center justify-between px-6">
+      <div class="text-yellow-600">Mevn</div>
+      <div>
+        <router-link class="no-underline text-gray-700" to="/auth/login">Sign in</router-link>
+        <router-link class="no-underline text-gray-700 border-2 p-1 ml-1 rounded-full border-brown" to="/auth/register">Join Now</router-link>
+      </div>
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
