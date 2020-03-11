b0d8d2814d07f9cc251577d0cb42c498c79f94ab
Oleksii Savchenko <o.savchenko@andersenlab.com>
2020-03-11 16:21:07 +0200

    designed page

--- client/pages/Main.vue
+++ client/pages/Main.vue
@@ -2,10 +2,11 @@
   <div>
     <div class="h-2 w-full bg-yellow-600"></div>
     <div class="h-12 w-full flex items-center justify-between px-6">
-      <div class="text-yellow-600">Mevn</div>
+      <router-link class="no-underline text-yellow-600" to="/">Mevn</router-link>
       <div>
         <router-link class="no-underline text-gray-700" to="/auth/login">Sign in</router-link>
-        <router-link class="no-underline text-gray-700 border-2 p-1 ml-1 rounded-full border-brown" to="/auth/register">Join Now</router-link>
+        <router-link class="no-underline text-gray-700 border-2 px-3 py-2 hover:text-gray-800 hover:border-gray-800 ml-1 rounded-full border-gray-600"
+                     to="/auth/register">Join Now</router-link>
       </div>
     </div>
 
--- client/pages/Register.vue
+++ client/pages/Register.vue
@@ -1,4 +1,23 @@
 <template>
-  <h1 class="text-center text-red-600">Register Component!</h1>
+  <div class="container my-16 mx-auto">
+    <div class="max-w-xs mx-auto">
+      <h2 class="text-center text-lg text-yellow-500">Register</h2>
+      <div class="w-full bg-white shadow-lg mt-5 rounded-sm p-12">
+        <input class="w-full text-xs bg-grey-200 p-3 focus:outline-none text-gray-800 mb-3"
+               type="text"
+               placeholder="Enter Your name"
+        >
+        <input class="w-full text-xs bg-grey-200 p-3 focus:outline-none text-gray-800 mb-3"
+               type="text"
+               placeholder="Enter Your email"
+        >
+        <input class="w-full text-xs bg-grey-200 p-3 focus:outline-none text-gray-800"
+               type="password"
+               placeholder="Enter Your password"
+        >
+        <button class="w-full mt-3 py-4 bg-blue-600 text-white text-sm rounded-sm focus:outline-none hover:bg-blue-500">Sign Up</button>
+      </div>
+    </div>
+  </div>
 </template>
 
